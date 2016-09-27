8b76144fac1696be15eb3f67532901e24224aac7
g3ol4d0 <g3ol4d0@hotmail.com>
2016-09-27 01:04:51 -0300

    Adding offsets
    Now checking for the type of the image , for only change the hex of the
    image , not the headers

--- br34k.py
+++ br34k.py
@@ -6,7 +6,6 @@ from PIL import Image , ImageTk
 
 root = Tkinter.Tk() 
 
-
 def clear() :
 	for widget in root.winfo_children():
 	    widget.destroy()
@@ -16,7 +15,6 @@ def key(event) :
 	br34k(sys.argv[2],random.randint(0,255))
 	sh0w(sys.argv[2])
 
-
 def reset(event) :
 	python = sys.executable
 	os.execl(python, python, * sys.argv)
@@ -24,8 +22,15 @@ def reset(event) :
 def br34k(fn,x) :
 	f = open(fn,"a+")
 	m = mmap.mmap(f.fileno(),0)
-
-	m[random.randint(0,len(m)-1)] = chr(x)
+	ft = fn.split(".")[-1]
+	s = 0
+	
+	if "jpeg" in ft or "jpg" in ft :
+		s = m.find(chr(int(0xff))+chr(int(0xda))) # start from the offset of 0xffda which is the start of the scan
+	if "png" in ft :
+		s = m.find("IDAT") # start from the offset of "IDAT" , I'm assuming there will be just one chunk , because I'm lazy
+	
+	m[random.randint(s,len(m)-1)] = chr(x)
 
 	m.close()
 	f.close()
@@ -49,7 +54,8 @@ def sh0w(fn) :
 
 def main() :
 	if ( len(sys.argv) < 3 ) :
-		print "Missing something . $ python br34k_1mg.py filename.jpg output.jpg"
+		print "Missing something !"
+		print "$ python br34k_1mg.py filename.jpg output.jpg"
 	else :
 		os.system("cp "+sys.argv[1]+" "+sys.argv[2]) # Command injection lols
 		sh0w(sys.argv[2])
